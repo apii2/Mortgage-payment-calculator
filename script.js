13503ced4b4331c72135813c2b7862c38115f968
apeksha <apekshrestha22@gmail.com>
2025-04-09 18:23:43 +0545

    added functionality for clear all button

--- script.js
+++ script.js
@@ -1,54 +1,66 @@
 document.addEventListener('DOMContentLoaded',function(){
-  let form = document.forms['myForm'];
+  try{
+    let form = document.forms['myForm'];
 
-  if(form){
-    form.addEventListener('submit',(e)=>{
-      e.preventDefault();
+    if(form){
+      form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        
+        let inputs = getData();
+        let calculation = new Calculation(inputs);
+        if(calculation.validate()){
+          return;
+        }
 
-      let inputs = getData();
-      let calculation = new Calculation(inputs);
-      if(calculation.validate()){
-        return;
-      }
+        let mortgage = calculation.calculate();
+        document.querySelector('.completed .mortgage_mon span').textContent=mortgage.mortgage_mon; 
+        document.querySelector('.completed .mortgage-tot span').textContent=mortgage.mortgage_tot; 
 
-      let mortgage = calculation.calculate();
-      document.querySelector('.completed .mortgage_mon span').textContent=mortgage.mortgage_mon; 
-      document.querySelector('.completed .mortgage-tot span').textContent=mortgage.mortgage_tot; 
+        document.querySelector('.empty').classList.add('hidden');
+        document.querySelector('.completed').classList.remove('hidden');
+      })
 
-      document.querySelector('.empty').classList.add('hidden');
-      document.querySelector('.completed').classList.remove('hidden');
-      
-      
-      // form.submit();
-    })
-  }
-  function getData(){
-    return [
-      {
-        name: 'amount',
-        value : form.querySelector('.amount input').value,
-        error : form.querySelector('.amount .err'),
-        element : form.querySelector('.amount div')
-      },
-      {
-        name: 'time',
-        value : form.querySelector('.time input').value,
-        error : form.querySelector('.time .err'),
-        element : form.querySelector('.time div')
-      },
-      {
-        name: 'rate',
-        value : form.querySelector('.rate input').value,
-        error : form.querySelector('.rate .err'),
-        element : form.querySelector('.rate div')
-      },
-      {
-        name: 'type',
-        value : form.querySelector('.type input:checked'),
-        error : form.querySelector('.type .err'),
-        element : form.querySelector('.type div')
-      }
-    ]
+      document.querySelector('.clear').addEventListener('click',()=>{
+        document.querySelector('.amount input').value = '';
+        document.querySelector('.time input').value = '';
+        document.querySelector('.rate input').value = '';
+        document.querySelector('.type input:checked').checked = false;
+
+        document.querySelector('.empty').classList.remove('hidden');
+        document.querySelector('.completed').classList.add('hidden');
+      });
+    }
+
+    function getData(){
+      return [
+        {
+          name: 'amount',
+          value : form.querySelector('.amount input').value,
+          error : form.querySelector('.amount .err'),
+          element : form.querySelector('.amount div')
+        },
+        {
+          name: 'time',
+          value : form.querySelector('.time input').value,
+          error : form.querySelector('.time .err'),
+          element : form.querySelector('.time div')
+        },
+        {
+          name: 'rate',
+          value : form.querySelector('.rate input').value,
+          error : form.querySelector('.rate .err'),
+          element : form.querySelector('.rate div')
+        },
+        {
+          name: 'type',
+          value : form.querySelector('.type input:checked'),
+          error : form.querySelector('.type .err'),
+          element : form.querySelector('.type div')
+        }
+      ]
+    }
+  }catch(err){
+    console.error(err);
   }
 });
 
